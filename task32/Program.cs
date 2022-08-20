82eb68ca4a1f6000adc8d94750941c7855ee7d20
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-20 11:21:27 +0300

    addTask

---
+++ task32/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы
+// замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+
+    return array;
+}
+
+
+int[] TempArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+
+int[] array = new int[12];
+array = NewArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
---
+++ task33/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+
+    return array;
+}
+
+
+string FindNumberInArray(int[] array, int userNumber)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == userNumber)
+            return ("Ваше число есть в массиве");
+    
+    }
+    return ("Вашего числа нет в массиве");
+}
+
+
+System.Console.WriteLine("Введите число которое будем искать");
+int userNumber = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine("Введите размер массива");
+int size = int.Parse(System.Console.ReadLine()!);
+
+int[] array = new int[size];
+array = NewArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+System.Console.WriteLine(FindNumberInArray(array,userNumber));
